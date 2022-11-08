4a03152758b89458f51f82027520cc99cfdb7b03
lucaswickens <42030483+lucaswickens@users.noreply.github.com>
2022-11-08 16:47:59 +0000

    Update quiz-logic.js

--- quiz-logic.js
+++ quiz-logic.js
@@ -1,5 +1,5 @@
 var Webflow = Webflow || [];
 Webflow.push(function () {
-  new AWF.MSF({hiddeButtonsOnSubmit: true, scrollTopOnStepChange: false, formSelector: '#quiz', nextSelector: '#nextButton', currentStepSelector: '#currentStep', backSelector: '#backButton', backText: [{step: '1', text: 'Go back'}, {step: '2', text: ' '}], nextText: [{step: '1', text: 'Take the quiz'}, {step: '2', text: 'Continue'}], completedPercentageSelector: '#completed', warningClass: 'warning', alertSelector: '#alertElement', alertText: 'Please complete all required fields'});
+  new AWF.MSF({hiddeButtonsOnSubmit: true, scrollTopOnStepChange: false, formSelector: '#quiz', nextSelector: '#nextButton', currentStepSelector: '#currentStep', backSelector: '#backButton', backText: [{step: '1', text: ' '}, {step: '2', text: ' Go back'}], nextText: [{step: '1', text: 'Take the quiz'}, {step: '2', text: 'Continue'}], completedPercentageSelector: '#completed', warningClass: 'warning', alertSelector: '#alertElement', alertText: 'Please complete all required fields'});
   
 });
