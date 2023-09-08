1586ddb3bae7c76faf75764bdd4e90861ccb7f84
lucaswickens <42030483+lucaswickens@users.noreply.github.com>
2023-09-08 13:54:10 +0100

    Update and rename main-logic-v4.0.2.js to main-logic-v4.0.3.js

--- main-logic-v4.0.3.js
+++ main-logic-v4.0.3.js
@@ -248,9 +248,13 @@ window.addEventListener("load", function () {
   // Display a custom error message if there's a pattern mismatch
   function customValidCheck(input, errorDiv) {
     if (input && errorDiv) {
+      console.log("input and error div found");
       input.addEventListener("blur", () => {
+        console.log(`Blur event for ${input.name} - checking validity...`);
         if (!input.validity.valid) {
+          console.log(`${input.name} is not valid, checking pattern...`);
           if (input.validity.patternMismatch) {
+            console.log(`${input.name} has a pattern mismatch - display error`);
             errorDiv.classList.remove("hidden");
           }
         } else {
